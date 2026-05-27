b2f5da77483dc843c9be5c259f131a85f00a2fc2
Arjunprasanth-tech <arjunprasanth369@gamail.com>
2026-05-27 15:48:26 +0530

    first project

--- src/main/java/com/example/proj10/Proj10Application.java
+++ src/main/java/com/example/proj10/Proj10Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Proj10Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Proj10Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Proj10Application.class, args);
+    }
 }
--- src/main/java/com/example/proj10/priority/PriorityRepository.java
+++ src/main/java/com/example/proj10/priority/PriorityRepository.java
@@ -2,5 +2,5 @@ package com.example.proj10.priority;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PriorityRepository extends JpaRepository<PriorityModel,Long> {
+public interface PriorityRepository extends JpaRepository<PriorityModel,String> {
 }
