f6819de159939e5ca8594e2592699f2dcd629b84
bibin <bibinmathew666666@gmail.com>
2026-05-29 02:42:50 +0530

    status final .....

--- src/main/java/com/example/proj10/priority/PriorityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.proj10.priority;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriorityRepository extends JpaRepository<PriorityModel,Long> {
-}
